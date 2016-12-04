1d60b0dd2b8c369a1d346653aad61835984896a5
“houbb” <1060732496@qq.com>
2016-12-04 22:39:02 +0800

    add mvc

---
+++ app-demo-web/src/main/java/README.md
@@ -0,0 +1,13 @@
+> org.springframework.web.SpringServletContainerInitializer cannot be cast to javax.servlet.ServletCon
+
+```xml
+<dependency> 
+<groupId>javax.servlet</groupId> 
+<artifactId>javax.servlet-api</artifactId> 
+<scope>provided</scope> 
+</dependency>
+```
+
+
+
+
---
+++ app-demo-web/src/main/java/com/ryo/app/demo/web/controller/ErrorController.java
@@ -0,0 +1,36 @@
+package com.ryo.app.demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author houbinbin
+ * @since 16/10/28
+ */
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+
+  private static final String BASE_PATH = "error";
+
+  @RequestMapping("/400")
+  public String Page400() {
+    return BASE_PATH+"/400";
+  }
+
+  @RequestMapping("/403")
+  public String Page403() {
+    return BASE_PATH+"/403";
+  }
+
+  @RequestMapping("/404")
+  public String Page404() {
+    return BASE_PATH+"/404";
+  }
+
+  @RequestMapping("/500")
+  public String Page500() {
+    return BASE_PATH+"/500";
+  }
+
+}
