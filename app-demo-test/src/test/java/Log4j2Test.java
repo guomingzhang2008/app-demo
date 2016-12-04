3b59beda0d27db4c205658f170b64430062b4d66
“houbb” <1060732496@qq.com>
2016-12-04 14:43:43 +0800

    add dal and test

---
+++ app-demo-test/src/test/java/Log4j2Test.java
@@ -0,0 +1,15 @@
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+/**
+ * @author houbinbin
+ * @since 16/12/4
+ */
+@Log4j2
+public class Log4j2Test {
+
+  @Test
+  public void test() {
+    log.info("test");
+  }
+}
